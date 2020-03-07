f285bf3607e56c7db6d2f794db8efdf031299c5a
coreman75 <coreman75@naver.com>
2020-03-07 13:01:48 +0900

    react first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render(){
+  return (
+    <div className="gray-backgroud">
+      <img src={logo} lat="log" />
+      <h2>Let's develop management system!</h2>
+    </div>
+  );
+  }
+}
+
+export default App;
+
