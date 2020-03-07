2661b44dd4c0910714c04f5ce95a341dec05c0a6
coreman75 <coreman75@naver.com>
2020-03-07 23:51:40 +0900

    create customer js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './component/Customer';
 import './App.css';
+
+const customers = [
+  {
+    'id' : '1',
+    'image' : 'https://placeimg.com/64/64/1',
+    'name' : 'hone gil dong',
+    'birthday' :  '20200305',
+    'gender' : 'man',
+    'job' : 'office worker'
+  },
+  {
+    'id' : '2',
+    'image' : 'https://placeimg.com/64/64/2',
+    'name' : 'kang dong gu',
+    'birthday' :  '20200305',
+    'gender' : 'man',
+    'job' : 'office worker'
+  },
+  {
+    'id' : '3',
+    'image' : 'https://placeimg.com/64/64/3',
+    'name' : 'kim min su',
+    'birthday' :  '20200305',
+    'gender' : 'man',
+    'job' : 'office worker'
+  }
+]
+
 
 class App extends Component {
   render(){
-  return (
-    <div className="gray-backgroud">
-      <img src={logo} lat="log" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+    return (
+      <div>
+        {
+        customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          )
+        })
+      }
+      </div>
+    )
   }
 }
 
 export default App;
 
+ 
